010b7e21eb5dcdee88f72c2e2092127a7a176f81
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-10-29 09:54:45 +0800

    changed the port

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ import {
 
 const app = express()
 const PORT = 3000
+const HOST = '0.0.0.0'
 
 const upload = multer({ dest: 'uploads/' })
 
@@ -59,6 +60,6 @@ app.post('/uploadFile', upload.single('file'), async (req, res) => {
     // }
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`)
 })
