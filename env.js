bb77d195d56e81114d0c64469f42b8de085b6b67
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-10-29 09:16:21 +0800

    Initial Commit

---
+++ env.js
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+
+export const SFTP_USER = assertValue(
+    process.env.SFTP_USER,
+    'SFTP_USER not configured'
+)
+
+export const SFTP_HOST = assertValue(
+    process.env.SFTP_HOST,
+    'SFTP_HOST not configured'
+)
+
+export const SFTP_PORT = assertValue(
+    process.env.SFTP_PORT,
+    'SFTP_PORT not configured'
+)
+
+export const RSA_PATH = assertValue(
+    process.env.RSA_PATH,
+    'RSA_PATH not configured'
+)
+
+export const PASS_PHRASE = assertValue(
+    process.env.PASS_PHRASE,
+    'PASS_PHRASE not configured'
+)
+
+export const SERVER_FILE_PATH = assertValue(
+    process.env.SERVER_FILE_PATH,
+    'SERVER_FILE_PATH not configured'
+)
+
+function assertValue(v, errorMessage) {
+    if (v === undefined) {
+        console.log(v)
+        throw new Error(errorMessage)
+    }
+    return v
+}
